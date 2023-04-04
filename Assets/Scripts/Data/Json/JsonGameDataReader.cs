ddf43580c401c12c6f8d208e81e327413b3d70f8
CesarVinken <39000090+CesarVinken@users.noreply.github.com>
2023-04-04 14:47:11 +0200

    Load hero data on Android

--- Assets/Scripts/Data/Json/JsonGameDataReader.cs
+++ Assets/Scripts/Data/Json/JsonGameDataReader.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class JsonGameDataReader : IJsonFileReader
 {
@@ -19,16 +20,28 @@ public class JsonGameDataReader : IJsonFileReader
     public SerialisableGameData ReadData<SerialisableGameData>()
     {
         string filePath = Path.Combine(Application.streamingAssetsPath, "game.json");
-
-        if (!File.Exists(filePath))
-        {
-            throw new Exception($"File game.json doesn't exist.");
-        }
-
-        string jsonContent = File.ReadAllText(filePath);
-
+        string jsonContent = GetJsonString(filePath);
         SerialisableGameData gameData = JsonUtility.FromJson<SerialisableGameData>(jsonContent);
-
         return gameData;
+    }
+
+    private string GetJsonString(string filePath)
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            UnityWebRequest loadingRequest = UnityWebRequest.Get(filePath);
+            loadingRequest.SendWebRequest();
+            while (!loadingRequest.isDone) ;
+
+            return loadingRequest.downloadHandler.text.Trim();
+        }
+        else
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new Exception($"File game.json doesn't exist.");
+            }
+            return File.ReadAllText(filePath);
+        }
     }
 }
